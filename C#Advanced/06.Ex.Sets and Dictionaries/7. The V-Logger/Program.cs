445c1de3373de9eb929a757f57210f360a5847b9
Yordan <daka7a11@abv.bg>
2021-05-09 12:01:36 +0300

    Added homework

---
+++ C#Advanced/06.Ex.Sets and Dictionaries/7. The V-Logger/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._The_V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vlogger> listOfVloggers = new List<Vlogger>();
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (input[0].ToLower() != "statistics")
+            {
+                if (input[1].ToLower() == "joined")
+                {
+                    string firstVlogger = input[0];
+                    if (!IsVloggerExist(listOfVloggers, firstVlogger))
+                    {
+                        listOfVloggers.Add(new Vlogger() { Name = firstVlogger });
+                    }
+                }
+                else if (input[1].ToLower() == "followed")
+                {
+                    string firstVlogger = input[0];
+                    string secondVlogger = input[2];
+                    if (IsVloggerExist(listOfVloggers, firstVlogger) && IsVloggerExist(listOfVloggers, secondVlogger) && firstVlogger != secondVlogger)
+                    {
+                        if (!IsFollowingExist(listOfVloggers, firstVlogger, secondVlogger))
+                        {
+                            foreach (var vlogger in listOfVloggers)
+                            {
+                                if (vlogger.Name == firstVlogger)
+                                {
+                                    vlogger.Followings.Add(secondVlogger);
+                                }
+                                else if (vlogger.Name == secondVlogger)
+                                {
+                                    vlogger.Followers.Add(firstVlogger);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            int count = 1;
+            Console.WriteLine($"The V-Logger has a total of {listOfVloggers.Count} vloggers in its logs.");
+            foreach (var vlogger in listOfVloggers.OrderByDescending(x => x.Followers.Count).ThenBy(x => x.Followings.Count))
+            {
+                Console.WriteLine($"{count}. {vlogger.Name} : {vlogger.Followers.Count} followers, {vlogger.Followings.Count} following");
+                if (count==1)
+                {
+                    foreach (var follower in vlogger.Followers.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+                count++;
+            }
+        }
+        static bool IsVloggerExist(List<Vlogger> vloggers, string name)
+        {
+            foreach (var vlogger in vloggers)
+            {
+                if (vlogger.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool IsFollowingExist(List<Vlogger> vloggers, string firstVlogger, string secondVlogger)
+        {
+            foreach (var vlogger in vloggers)
+            {
+                if (vlogger.Name == firstVlogger)
+                {
+                    foreach (var following in vlogger.Followings)
+                    {
+                        if (following == secondVlogger)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    public class Vlogger
+    {
+        public Vlogger()
+        {
+            Followers = new List<string>();
+            Followings = new List<string>();
+        }
+        public string Name { get; set; }
+        public List<string> Followers { get; set; }
+        public List<string> Followings { get; set; }
+    }
+}
---
+++ C#Advanced/06.Ex.Sets and Dictionaries/8. Ranking/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> contests = new Dictionary<string, string>();
+            Dictionary<string, Dictionary<string, int>> candidates = new Dictionary<string, Dictionary<string, int>>();
+            string[] input = Console.ReadLine().Split(":",StringSplitOptions.RemoveEmptyEntries);
+            while (input[0].ToLower()!="end of contests")
+            {
+                string contest = input[0];
+                string password = input[1];
+                contests.Add(contest,password);
+                input = Console.ReadLine().Split(":", StringSplitOptions.RemoveEmptyEntries);
+            }
+            input = Console.ReadLine().Split("=>",StringSplitOptions.RemoveEmptyEntries);
+            while (input[0].ToLower()!="end of submissions")
+            {
+                string content = input[0];
+                string password = input[1];
+                string name = input[2];
+                int points = int.Parse(input[3]);
+                if (contests.ContainsKey(content)) // IsContentExist
+                {
+                    if (contests[content]==password) // IsPasswordCorrect
+                    {
+                        if (candidates.ContainsKey(name)) // IsCandidateExist
+                        {
+                            if (candidates[name].ContainsKey(content))
+                            {
+                                if (candidates[name][content]<points)
+                                {
+                                    candidates[name][content] = points;
+                                }
+                            }
+                            else
+                            {
+                                candidates[name].Add(content, points);
+                            }
+                        }
+                        else
+                        {
+                            candidates.Add(name, new Dictionary<string, int>());
+                            candidates[name].Add(content,points);
+                        }
+                    }
+                }
+                input = Console.ReadLine().Split("=>", StringSplitOptions.RemoveEmptyEntries);
+            }
+            string bestCandidate = string.Empty;
+            int bestPoints = 0;
+            foreach (var candidate in candidates)
+            {
+                int totalPoints = 0;
+                foreach (var content in candidate.Value)
+                {
+                    totalPoints += content.Value;
+                }
+                if (totalPoints>bestPoints)
+                {
+                    bestCandidate = candidate.Key;
+                    bestPoints = totalPoints;
+                }
+            }
+            Console.WriteLine($"Best candidate is {bestCandidate} with total {bestPoints} points.");
+            Console.WriteLine("Ranking:");
+            foreach (var candidate in candidates.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(candidate.Key);
+                foreach (var content in candidate.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {content.Key} -> {content.Value}");
+                }
+            }
+        }
+    }
+}
