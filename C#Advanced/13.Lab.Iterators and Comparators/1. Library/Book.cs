e962fae315aca80b38c02ebd5f984a4e0256f8be
Yordan <daka7a11@abv.bg>
2021-06-03 15:27:55 +0300

    Added homework

---
+++ C#Advanced/13.Lab.Iterators and Comparators/1. Library/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+           this.authors = authors.ToList();
+        }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        private List<string> authors;
+
+        public int CompareTo(Book other)
+        {
+            int result = Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
---
+++ C#Advanced/13.Lab.Iterators and Comparators/1. Library/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2004, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2005, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+
+        }
+    }
+}
